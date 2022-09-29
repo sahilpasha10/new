18243590bed13a142426c161f84fad2c3b616baa
sahilpasha10 <114655358+sahilpasha10@users.noreply.github.com>
2022-09-29 16:01:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var s = 10;
+        var a = 20;
+        
+
+        document.write(s+a);
+        document.write("<br/>");
+        document.write(s-a);
+        document.write("<br/>");
+        document.write(s/a);
+        document.write("<br/>");
+        document.write(s*a);
+        document.write("<br/>");
+        document.write(s%a);
+        document.write("<br/>");
+
+        var z= s + a;
+        document.write(z);
+        document.write("<br/>");
+
+        var h="sahil";
+        var i ="pasha";
+        var o= h+i;
+        document.write(o);
+        
+        
+        var s = 10;
+        var a = 20;
+        
+
+        document.write(s+a);
+        document.write("<br/>");
+        document.write(s-a);
+        document.write("<br/>");
+        document.write(s/a);
+        document.write("<br/>");
+        document.write(s*a);
+        document.write("<br/>");
+        document.write(s%a);
+        document.write("<br/>");
+
+        var z= s + a;
+        document.write(z);
+        document.write("<br/>");
+
+        var h="sahil";
+        var i ="pasha";
+        var o= h+i;
+        document.write(o);
+        
+        
+        
---
+++ index2.js
@@ -0,0 +1,57 @@
+var s = 40;
+        var a = 40;
+        
+
+        document.write(s+a);
+        document.write("<br/>");
+        document.write(s-a);
+        document.write("<br/>");
+        document.write(s/a);
+        document.write("<br/>");
+        document.write(s*a);
+        document.write("<br/>");
+        document.write(s%a);
+        document.write("<br/>");
+
+        var z= s + a;
+        document.write(z);
+        document.write("<br/>");
+
+        var h="pasha";
+        var i ="sahil";
+        var o= h+i;
+        document.write(o);
+        
+        
+        var s = 10;
+        var a = 20;
+        
+
+        document.write(s+a);
+        document.write("<br/>");
+        document.write(s-a);
+        document.write("<br/>");
+        document.write(s/a);
+        document.write("<br/>");
+        document.write(s*a);
+        document.write("<br/>");
+        document.write(s%a);
+        document.write("<br/>");
+
+        var z= s + a;
+        document.write(z);
+        document.write("<br/>");
+
+        var h="sahil";
+        var i ="pasha";
+        var o= h+i;
+        document.write(o);
+        document.write("<br/>");
+
+        var u="jayanth";
+var w="anything";
+var r=u+w;
+document.write(r);
+        
+        
+        
